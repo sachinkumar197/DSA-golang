09309a4510c5c5e985045b983bf6d948da62cbd1
sachin <sachin.kumar@vayana.com>
2022-10-21 01:50:10 +0530

    added URL for the problem statement

--- arrayRotation/tempArray.go
+++ arrayRotation/tempArray.go
@@ -1,3 +1,4 @@
+//https://www.geeksforgeeks.org/array-rotation/
 // Problem : Given an array of integers arr[] of size N and an integer, the task is to rotate the array elements to the left by d positions.
 
 // Using Temp array
