5dbe435db08a2414029ca667dea7e7775d39138e
sachin <sachin.kumar@vayana.com>
2022-10-21 01:47:21 +0530

    basic array rotation with temp array

--- arrayRotation/arrayRotation.go
+++ arrayRotation/arrayRotation.go
@@ -1,7 +1,20 @@
+// Problem : Given an array of integers arr[] of size N and an integer, the task is to rotate the array elements to the left by d positions.
+
+// Using Temp array
+
 package main
 
 import "fmt"
 
+func rotateArray(_list []int, d int) []int {
+	var tempArray []int
+	tempArray = _list[d:]
+	tempArray = append(tempArray, _list[:d]...)
+	_list = tempArray
+	return _list
+}
+
 func main() {
-	fmt.Println("Initial Commit")
+	input := []int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Println(rotateArray(input, 2))
 }
