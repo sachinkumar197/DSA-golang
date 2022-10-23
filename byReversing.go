cc36aa09ba53f0790492f9aad377ad900d18ff86
sachin <sachin.kumar@vayana.com>
2022-10-23 14:24:57 +0530

    added reversing the array approach

---
+++ byReversing.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func reverse(xs []int) []int {
+	for i := 0; i < len(xs)/2; i++ {
+		temp1 := xs[len(xs)-i-1]
+		xs[len(xs)-i-1] = xs[i]
+		xs[i] = temp1
+	}
+	return xs
+}
+
+func rotateArray(xs []int, d int) []int {
+	x := reverse(xs)
+	fmt.Println(x)
+	ind := len(xs) - d
+	leftArray := reverse(x[:ind])
+	fmt.Println(leftArray)
+	rightArray := reverse(x[ind:])
+	fmt.Println(rightArray)
+	return append(leftArray, rightArray...)
+}
+
+func main() {
+	_list := []int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Println(rotateArray(_list, 2))
+}
