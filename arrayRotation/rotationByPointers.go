7e4b54d31ea3049c4afd125c662c1e8fddf23ee6
sachin <sachin.kumar@vayana.com>
2022-10-25 00:41:21 +0530

    Array rotation by using pointer

---
+++ arrayRotation/rotationByPointers.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func rotateByOne(xs []int) []int {
+	i := 0
+	j := len(xs) - 1
+	for i != j {
+		xs[i], xs[j] = xs[j], xs[i]
+		i += 1
+	}
+	return xs
+}
+
+func main() {
+	_list := []int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Println(rotateByOne(_list))
+}
