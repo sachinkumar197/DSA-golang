825b7bfdbe069914c0c107b9a0ac9d268f091bc8
sachin <sachin.kumar@vayana.com>
2022-11-13 20:01:18 +0530

    deletion in a sorted array

--- arrayDeletion/arrayDeletionInSortedArray.go
+++ arrayDeletion/arrayDeletionInSortedArray.go
@@ -6,6 +6,8 @@ import "fmt"
 
 func binarySearch(xs []int, start int, end int, key int) int {
 	mid := (start + end) / 2
+	_var := fmt.Sprintf("start: %d, end: %d, mid: %d", start, end, mid)
+	fmt.Println(_var)
 	if xs[mid] == key {
 		return mid
 	} else if end <= start || start >= end {
@@ -18,7 +20,18 @@ func binarySearch(xs []int, start int, end int, key int) int {
 	return -1
 }
 
+func deleteElem(xs []int, d int) []int {
+	_index := binarySearch(xs, 0, len(xs)-1, d)
+	if _index != -1 {
+		for i := _index; i < len(xs)-1; i++ {
+			xs[i] = xs[i+1]
+		}
+		return xs[:len(xs)-1]
+	}
+	return xs
+}
+
 func main() {
 	_list := []int{1, 2, 3, 4, 5, 6, 7}
-	fmt.Println(binarySearch(_list, 0, len(_list), 6))
+	fmt.Println(deleteElem(_list, 5))
 }
